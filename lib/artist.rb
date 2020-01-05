539dc515523ed758a6587d37b7c538cb82400258
Adrian Mohabir <mohabiradrian@gmail.com>
2020-01-05 17:13:07 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,44 @@
+require 'pry'
+class Artist 
+  
+  attr_accessor :name, :songs
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @songs = []
+    save
+  end 
+  
+  def songs
+    Song.all.select do |song|
+    song.artist == self 
+    end 
+  end 
+    
+  
+ def add_song(song)
+   song.artist = self 
+ end 
+  
+  def save 
+    @@all << self 
+    self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.find_or_create_by_name(name)
+    self.all.find {|artist| artist.name == name} || Artist.new(name)
+    end 
+    
+  def print_songs
+    songs.each do |song|
+      puts song.name 
+    end 
+  end 
+  
+end 
